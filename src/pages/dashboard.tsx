90423b5cf96a6630c1b8101b27a1da0cff9aceca
Joshua Soong <joshua@soong.nz>
2023-06-07 02:27:44 +1200

    added nav and maps

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,14 +4,67 @@ import { useSession } from 'next-auth/react'
 import { motion } from 'framer-motion'
 import { dashboardText, letter } from '@/utils/motionText'
 import Penguins from '../../public/penguins.svg'
+import { useEffect, useState } from 'react'
+import { signOut } from 'next-auth/react'
+import Map, { Marker } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import { parse } from 'path'
 
 const Dashboard: NextPage = () => {
   const { data: session, status } = useSession()
 
+  const [countdown, setCountdown] = useState('Countdown')
+
+  useEffect(() => {
+    const targetDate = new Date(process.env.NEXT_PUBLIC_DATE).getTime()
+
+    const interval = setInterval(() => {
+      const now = new Date().getTime()
+      const distance = targetDate - now
+
+      if (distance <= 0) {
+        clearInterval(interval)
+        setCountdown("It's here!")
+      } else {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+        setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`)
+      }
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   if (status === 'authenticated') {
     return (
-      <div data-theme='green'>
-        <main className='flex flex-col items-center justify-center h-[calc(100dvh)] font-body font-light text-neutral text-lg tracking-wide min-w-[360px] min-h-[750px] px-8 lg:my-0'>
+      <div data-theme='green' className='font-body font-light text-neutral text-lg tracking-wide'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+          className='navbar sticky top-0 px-10 py-8 font-display'
+        >
+          <div className='navbar-start'>
+            <button className='btn' onClick={() => signOut()}>
+              Log Out
+            </button>
+          </div>
+
+          <div className='navbar-end'>
+            <button className='btn'>RSVP</button>
+          </div>
+        </motion.div>
+        <main className='flex flex-col items-center h-[calc(100dvh)] min-w-[360px] min-h-[750px] px-8 lg:my-0'>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1 }}
+            className='mt-[-4.25em] mb-10 font-display font-bold text-base'
+          >
+            {countdown}
+          </motion.div>
           <div className='relative flex items-center justify-center w-full h-fit'>
             <motion.div
               className='absolute flex items-center justify-center w-[30%] mt-4 sm:mt-8 md:mt-9'
@@ -30,6 +83,7 @@ const Dashboard: NextPage = () => {
                   ease: 'easeInOut',
                 }}
                 strokeWidth={12}
+                strokeLinecap='round'
                 strokeDasharray='0 1'
                 fill='none'
                 stroke='#fff'
@@ -58,7 +112,7 @@ const Dashboard: NextPage = () => {
             variants={dashboardText}
             initial='hidden'
             animate='visible'
-            className='justify-center font-heading font-light tracking-wide text-5xl sm:text-6xl md:text-7xl lg:text-8xl'
+            className='justify-center font-heading font-light tracking-wide text-5xl sm:text-6xl md:text-7xl lg:text-8xl mb-10'
           >
             {'Hi, '.split('').map((char, index) => {
               return (
@@ -87,6 +141,40 @@ const Dashboard: NextPage = () => {
               )
             })}
           </motion.h1>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 1.2 }}
+            className='max-w-[900px] mb-10'
+          >
+            Location: {process.env.NEXT_PUBLIC_LOCATION}
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 1.2 }}
+            className='max-w-[900px] mb-10'
+          >
+            <Map
+              reuseMaps
+              mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+              mapLib={import('mapbox-gl')}
+              initialViewState={{
+                latitude: parseFloat(process.env.NEXT_PUBLIC_MAP_COORDS.split(',')[0]),
+                longitude: parseFloat(process.env.NEXT_PUBLIC_MAP_COORDS.split(',')[1]),
+                zoom: 10,
+              }}
+              style={{ width: 600, height: 600 }}
+              mapStyle='mapbox://styles/spren/clikciyg5000001q447fchmm7'
+            >
+              <Marker
+                color='#31553d'
+                latitude={parseFloat(process.env.NEXT_PUBLIC_MARKER_COORDS.split(',')[0])}
+                longitude={parseFloat(process.env.NEXT_PUBLIC_MARKER_COORDS.split(',')[1])}
+                anchor='bottom'
+              ></Marker>
+            </Map>
+          </motion.div>
         </main>
       </div>
     )
